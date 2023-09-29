8f963111765170dcea5174bdee3411a2df54fe22
JoaoHFerreira <jaycops@gmail.com>
2023-09-29 17:02:44 -0300

    add two new routes

--- api.py
+++ api.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from datetime import datetime
+
 
 app = FastAPI()
+
+def _get_current_date():
+    return datetime.now().strftime("%Y-%m-%d")
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/fetch_news/{date}")
+def fetch_news(date: str):
+    return {"date": date}
+
+
+@app.get("/fetch_news/")
+def fetch_news():
+    date = _get_current_date()
+    
+    return {"date": date}
+    
